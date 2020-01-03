1b70aeabc320bf51eec0104aaa95075e09e220f9
Bohdan Mykytey <bohdan.mykytey@gmail.com>
2020-01-03 12:18:46 -0500

    added showContact() toggle swithcing

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -5,41 +5,41 @@ class Contact extends Component {
   constructor() {
     super();
     this.state = {
-      showContactInfo: true //set state
+      showContactInfo: false //set state
     };
 
     // no function binding nessesary when using arrow functions for onClick
     // on event change etc
   }
-  
+
   //proptypes validates the data type(declares what
   //it should be like string, object etc)
   static propTypes = {
     contact: PropTypes.object.isRequired
   };
 
-  onShowClick = (event) => {
+  onShowClick = event => {
     this.setState(
-      {showContactInfo: !this.state.showContactInfo} //toggle state
-    )
-  }
-
+      { showContactInfo: !this.state.showContactInfo } //toggle state
+    );
+  };
 
   render() {
     const { name, email, phone } = this.props.contact; //destructuring - same as this.props.contact accept cleaner and with less code
+    const { showContactInfo } = this.state;
+
     return (
       <div className="card card-body mb-3">
         <h4>
-          {name} 
-          <i 
-            onClick={this.onShowClick} 
-            className="fas fa-sort-down" 
-          />
+          {name}
+          <i onClick={this.onShowClick} className="fas fa-sort-down" />
         </h4>
-        <ul className="list-group">
-          <li className="list-group-item">{email}</li>
-          <li className="list-group-item">{phone}</li>
-        </ul>
+        {showContactInfo ? ( //terinary operator for toggling showContactInfo()
+          <ul className="list-group">
+            <li className="list-group-item">{email}</li>
+            <li className="list-group-item">{phone}</li>
+          </ul>
+        ) : null}
       </div>
     );
   }
--- src/Components/Contacts.js
+++ src/Components/Contacts.js
@@ -22,8 +22,8 @@ class Contacts extends Component {
         },
         {
           id: 3,
-          name: "Harry Johnson",
-          email: "hjohnson@gmail.com",
+          name: "Mike Smith",
+          email: "msmith@gmail.com",
           phone: "555-132-4576"
         }
       ]
@@ -33,9 +33,12 @@ class Contacts extends Component {
   render() {
     const { contacts } = this.state; //destructuring using curly braces
 
+    // React.Fragment gets rid of unnesesary elemnts on the dom like an extra <div></div>
     return (
       <React.Fragment>
-        {contacts.map(contact  => ( //pulling values out of state and mapping over them
+        {contacts.map((
+          contact //pulling values out of state and mapping over them
+        ) => (
           <Contact
             key={contact.id} //again, react likes id's
             contact={contact} //whats being rendered
