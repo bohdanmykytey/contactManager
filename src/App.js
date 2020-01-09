866d091e5f94408cf47164c4835cc06236416682
Bohdan Mykytey <bohdan.mykytey@gmail.com>
2020-01-09 12:45:30 -0500

    brough in react router dom and added about page

--- src/App.js
+++ src/App.js
@@ -2,21 +2,28 @@ import React, { Component } from "react";
 import "./App.css";
 import Contacts from './Components/contacts/Contacts';
 import Header from './Components/layout/Header';
+import About from './Components/pages/About';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Provider } from "./context";
 import AddContact from './Components/layout/AddContact'
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
 
 class App extends Component {
   render() {
     return (
       <Provider>
+        <Router >
         <div className="App">
           <Header branding="Contact Manager" />
           <div className="container">
-            <AddContact />
-            <Contacts />
+            <Switch>
+              <Route exact path='/' component={Contacts} />
+              <Route exact path='/about' component={About} />
+            </Switch>
           </div>
         </div>
+        </Router>
       </Provider>
     );
   }
